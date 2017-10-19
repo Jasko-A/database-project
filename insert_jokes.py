08f45f3481570581862f0a934ab388d5c576e4b2
nick-webb <njwebb@ucdavis.edu>
2017-10-19 15:31:50 -0700

    fixed csv script

--- insert_jokes.py
+++ insert_jokes.py
@@ -15,7 +15,13 @@ def insert_jokes(filename='jokes.csv'):
 	with open(filename, 'r') as csvfile:
 		reader = csv.reader(csvfile)
 		num = 0
+		firstline = True
 		for row in reader:
+
+			if firstline:
+				firstline = False
+				continue
+
 			joke_text = row[TEXT]
 			joke_category = row[CATEGORY].lower()
 			joke_subj = row[SUBJECT]
